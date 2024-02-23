9733afba78e27431b5b2d3542e540e295669b9b6
Eric Chen <kych@berkeley.edu>
2024-02-23 21:46:08 +0000

    Add logging for request and response packets

--- sgc_launch/sgc_launch/sgc_swarm.py
+++ sgc_launch/sgc_launch/sgc_swarm.py
@@ -3,7 +3,6 @@ import yaml
 import pprint
 import random
 
-
 class Node:
     def __init__(self, address, parent=None):
         self.address = address
@@ -447,7 +446,7 @@ class SGC_Swarm:
             receiver_url,
             connection_type,
         ):
-            sleep(1)
+            sleep(random.randint(1, 10) * 0.5) # TODO: a hack to prevent sending at the same time 
             self.logger.info(
                 f"send routing request service {[addr, topic_name, topic_type, source_or_destination, sender_url, receiver_url, connection_type]}"
             )
