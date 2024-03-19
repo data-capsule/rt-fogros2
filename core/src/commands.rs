d6f9630f3227b7c3df4c97f53e7bec3909d2b62a
Kaiyuan Chen <kych@berkeley.edu>
2024-03-19 17:03:41 +0000

    revive webrtc

--- core/src/commands.rs
+++ core/src/commands.rs
@@ -3,7 +3,6 @@ extern crate tokio_core;
 
 use crate::ebpf_routing_manager::ebpf_routing_manager;
 use crate::{api_server::ros_api_server, util::get_non_existent_ip_addr};
-use crate::service_manager_udp::ros_service_manager;
 use crate::topic_manager::ros_topic_manager;
 use futures::future;
 
@@ -14,7 +13,7 @@ use utils::error::Result;
 /// inspired by https://stackoverflow.com/questions/71314504/how-do-i-simultaneously-read-messages-from-multiple-tokio-channels-in-a-single-t
 /// TODO: later put to another file
 #[tokio::main]
-async fn router_async_loop() {
+async fn ebpf_router_async_loop() {
     let (topic_request_tx, topic_request_rx) = mpsc::unbounded_channel();
 
     let (service_request_tx, service_request_rx) = mpsc::unbounded_channel();
@@ -28,10 +27,37 @@ async fn router_async_loop() {
 
     let ros_service_manager_handle = tokio::spawn(async move {
         std::thread::sleep(std::time::Duration::from_millis(1000));
-        ros_service_manager(ebpf_request_tx, service_request_rx).await;
+        crate::service_manager_udp::ros_service_manager(ebpf_request_tx, service_request_rx).await;
     });
 
     future_handles.push(tokio::spawn(ebpf_routing_manager(ebpf_request_rx)));
+    future_handles.push(ros_service_manager_handle);
+
+    let ros_api_server_handle = tokio::spawn(ros_api_server(topic_request_tx, service_request_tx));
+    future_handles.push(ros_api_server_handle);
+
+    future::join_all(future_handles).await;
+}
+
+
+/// inspired by https://stackoverflow.com/questions/71314504/how-do-i-simultaneously-read-messages-from-multiple-tokio-channels-in-a-single-t
+/// TODO: later put to another file
+#[tokio::main]
+async fn webrtc_router_async_loop() {
+    let (topic_request_tx, topic_request_rx) = mpsc::unbounded_channel();
+
+    let (service_request_tx, service_request_rx) = mpsc::unbounded_channel();
+
+    let mut future_handles = Vec::new();
+
+    let ros_topic_manager_handle = tokio::spawn(ros_topic_manager(topic_request_rx));
+    future_handles.push(ros_topic_manager_handle);
+
+    let ros_service_manager_handle = tokio::spawn(async move {
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+        crate::service_manager_webrtc::ros_service_manager(service_request_rx).await;
+    });
+
     future_handles.push(ros_service_manager_handle);
 
     let ros_api_server_handle = tokio::spawn(ros_api_server(topic_request_tx, service_request_tx));
@@ -47,7 +73,7 @@ pub fn router() -> Result<()> {
 
     // NOTE: uncomment to use pnet
     // libpnet::pnet_proc_loop();
-    router_async_loop();
+    webrtc_router_async_loop();
 
     Ok(())
 }
