3ec8406718f791c7a1b7a2a2a21119866a405f75
Eric Chen <kych@berkeley.edu>
2024-06-20 23:51:14 +0000

    make logs more expressive

--- fog_rs/fogrs-core/src/commands.rs
+++ fog_rs/fogrs-core/src/commands.rs
@@ -43,8 +43,9 @@ use tokio::sync::mpsc;
 /// TODO: later put to another file
 #[tokio::main]
 async fn webrtc_router_async_loop() {
-    console_subscriber::init();
-    // env_logger::init();
+    // console_subscriber::init();
+    env_logger::builder()
+    .format_timestamp_micros().init();
     let (topic_request_tx, topic_request_rx) = mpsc::unbounded_channel();
 
     let (service_request_tx, service_request_rx) = mpsc::unbounded_channel();
